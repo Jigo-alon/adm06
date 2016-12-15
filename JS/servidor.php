6b6db022cb4f7d40e189392172ba48c58ca60936
Jonathan MirCha <jonmircha@gmail.com>
2016-12-14 21:34:39 -0600

    Clase 10 Interacción

--- JS/servidor.php
+++ JS/servidor.php
@@ -2,3 +2,11 @@
 echo '<h1>Hola Mundo con PHP</h1>';
 echo '<p>Como AJAX es asíncrono, me permite consultar el servidor sin necesidad de recargar el navegador</p>';
 echo 'irfbiergfuerifbuierfh';
+echo 'irfbiergfuerifbuierfh';
+echo '<br>';
+
+for ($i=1; $i < 7; $i++) { 
+	echo "<h$i>ENCABEZADO de tipo $i</h$i>"; //comilla doble si interpola variables
+	echo '$i'; //comilla sencilla no interpola variables
+	echo '<br>';
+}
