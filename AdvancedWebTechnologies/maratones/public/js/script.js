e836615f6dd216c751e4f62a5ae4c784a846f51e
Jonathan MirCha <jonmircha@gmail.com>
2017-04-05 20:36:32 -0500

    Clase 5 AWT

---
+++ AdvancedWebTechnologies/maratones/public/js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var tecnologia = 'Babel',
+    hola = 'Hola Mundo con ' + tecnologia;
+
+document.addEventListener('DOMContentLoaded', function () {
+  return alert(hola);
+});
---
+++ AdvancedWebTechnologies/maratones/src/js/script.js
@@ -0,0 +1,7 @@
+
+
+let tecnologia = 'Babel',
+  hola = `Hola Mundo con ${tecnologia}`;
+
+
+document.addEventListener('DOMContentLoaded', () => alert(hola) )
