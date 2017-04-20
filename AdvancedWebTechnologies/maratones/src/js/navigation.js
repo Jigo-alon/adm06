1d7b82d672e4632a4e91f323df7eab9538375d91
Jonathan MirCha <jonmircha@gmail.com>
2017-04-19 21:06:00 -0500

    Clase 9 AWT

--- AdvancedWebTechnologies/maratones/src/js/navigation.js
+++ AdvancedWebTechnologies/maratones/src/js/navigation.js
@@ -5,9 +5,21 @@
     hamburger = d.querySelector('.hamburger'),
     mq = w.matchMedia('(min-width: 64em)')
 
+  function closePanel(mq) {
+    if (mq.matches) {
+      panel.classList.remove('is-active')
+      hamburger.classList.remove('is-active')
+    }
+
+    console.log(mq.matches)
+  }
+
   panelBtn.addEventListener('click', e => {
     e.preventDefault()
     panel.classList.toggle('is-active')
     hamburger.classList.toggle('is-active')
   });
+
+  mq.addListener(closePanel) //es un manejador que detecta la mq
+  closePanel(mq) //la función se ejecuta al cargar el documento
 })(document, window);
